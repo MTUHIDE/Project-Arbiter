aa0573e742869f2c6b5168a0f3695260aff10045
Josh Eckert <jweckert@mtu.edu>
2018-10-25 20:21:28 -0400

    initial commit

---
+++ .spyder-py3/history.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Tue Oct 16 13:35:56 2018)---
+runfile('C:/Users/joshe/.spyder-py3/hello.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Tue Oct 16 16:46:42 2018)---
+runfile('C:/Users/joshe/.spyder-py3/hello.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Tue Oct 16 17:14:29 2018)---
+runfile('C:/Users/joshe/.spyder-py3/hello.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Tue Oct 16 18:04:26 2018)---
+runfile('C:/Users/joshe/.spyder-py3/hello.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Tue Oct 16 18:29:17 2018)---
+runfile('C:/Users/joshe/.spyder-py3/hello.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Wed Oct 17 17:18:03 2018)---
+runfile('C:/Users/joshe/.spyder-py3/hello.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Sun Oct 21 15:55:35 2018)---
+runfile('C:/Users/joshe/.spyder-py3/hello.py', wdir='C:/Users/joshe/.spyder-py3')
+runfile('C:/Users/joshe/.spyder-py3/xmlParserV2.0.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Sun Oct 21 17:33:27 2018)---
+runfile('C:/Users/joshe/.spyder-py3/xmlParserV2.0.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Wed Oct 24 15:05:15 2018)---
+runfile('C:/Users/joshe/.spyder-py3/xmlParserV2.0.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Wed Oct 24 16:45:31 2018)---
+runfile('C:/Users/joshe/.spyder-py3/xmlParserV2.0.py', wdir='C:/Users/joshe/.spyder-py3')
+
+## ---(Thu Oct 25 20:05:26 2018)---
+runfile('C:/Users/joshe/.spyder-py3/xmlParserV2.0.py', wdir='C:/Users/joshe/.spyder-py3')
---
+++ .spyder-py3/xmlParserV2.0.py
@@ -0,0 +1,38 @@
+import xml.dom.minidom
+document = "test3.xml"
+
+dom = xml.dom.minidom.parse(document)
+
+#Examines a node's contents and converts it to a string
+def getNodeText(node):
+
+    nodelist = node.childNodes
+    result = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            result.append(node.data)
+    return ''.join(result)
+
+#Returns a block's attribute
+def handleBlockType(block):
+    print(block.getAttribute("s"))
+
+#Returns each block and its contents
+def handleBlocks(blocks):
+    for block in blocks:
+        handleBlockType(block)
+        l = block.getElementsByTagName("l")
+        if len(l) > 0:
+            for node in l:
+                handleLTagParameters(node)
+        
+#Returns text within a tag l
+def handleLTagParameters(l):
+    print(getNodeText(l))
+
+#Parses XML to Python given dom input
+def parseXML(xml):
+    blocks = dom.getElementsByTagName("block")
+    handleBlocks(blocks)
+
+parseXML(dom)
